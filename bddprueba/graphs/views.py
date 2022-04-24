432476a2bbb2a8ee26c75b80729d891d78068839
silvyc <smchain@uninorte.edu.co>
2022-04-23 21:48:01 -0500

    models actualizados

--- bddprueba/graphs/views.py
+++ bddprueba/graphs/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from graphs.models import CovidCasesDaily
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    dailys = CovidCasesDaily.objects.all().values('deaths')
+    deaths_list = (list(dailys))
+    deaths_dicts = dict()
+    deaths_dicts['deaths'] = deaths_list
+    return JsonResponse(deaths_dicts)
